6b3302ba532a22dfee40205235a32fa3bc343e7a
jawid-fakhari <63859337+jawid-fakhari@users.noreply.github.com>
2021-06-11 19:31:24 +0200

    skills completed

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 var x = document.getElementById("menu");
 
+
 function dropDown(e) {
     e.classList.toggle("change");
     if (x.style.display === "flex") {
-      x.style.display = "none";
+        x.style.display = "none";
     } else {
-      x.style.display = "flex"; 
+        x.style.display = "flex";
     }
-  }
+}
